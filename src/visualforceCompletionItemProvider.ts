2e5250410c8024a7833e3e3470805d5ffbf3bcbe
André Thériault <atheriault2@coveo.com>
2016-06-26 03:52:19 -0400

    Now build the Completion list only once

--- src/visualforceCompletionItemProvider.ts
+++ src/visualforceCompletionItemProvider.ts
@@ -2,17 +2,19 @@ import vscode = require('vscode')
 import {visualforce} from './refs/visualforce'
 
 export class VisualforceCompletionItemProvider implements vscode.CompletionItemProvider {
-  private autoCompleteData = visualforce;
+  private autoCompletionItems: vscode.CompletionItem[] = [];
+
+  public constructor () {
+    for (var tag in visualforce) {
+      this.autoCompletionItems.push(new vscode.CompletionItem(tag));
+    }
+  }
 
   public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
+    var _this = this;
+
     return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
-      var autoCompletionItems: vscode.CompletionItem[] = [];
-
-      for (var tag in this.autoCompleteData) {
-        autoCompletionItems.push(new vscode.CompletionItem(tag));
-      }
-
-      resolve(autoCompletionItems);
+      resolve(_this.autoCompletionItems);
     });
   }
 }
