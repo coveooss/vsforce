0a6a5d98def8e0d5708b5d60fcd7bd8d13b0c41c
Lucien Benie <lbenie@coveo.com>
2016-08-04 22:46:03 -0400

    adds comments

--- src/commands/showDiffCommand.ts
+++ src/commands/showDiffCommand.ts
@@ -3,9 +3,20 @@ import vscode = require('vscode');
 import {Command} from './command';
 import {buildSalesforceUri, getFileNameFromUri} from '../utils';
 
+/**
+ * This class instantiate a diff command
+ */
 export class ShowDiffCommand implements Command {
+  /**
+   * Creates a new diff command
+   */
   constructor() {}
 
+  /**
+   * Implements command from {@link Command}
+   *
+   * @param {vscode.Uri} uri Context from the right click `Compare with Salesforce`
+   */
   public Command(uri: vscode.Uri) {
     // split[0] == filename
     // split[1] == file extension
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,15 @@ export function getFileNameFromUri(uri: vscode.Uri): string {
   return uri.path.replace(/^.*[\\\/]/, '')
 }
 
+/**
+ * This will build a saleforce uri string for SOQL
+ * {@link buildApexComponentQuery}
+ *
+ * @param {string} component saleforce component name
+ * @param {string} extension salesforce component extension
+ *
+ * @return {string} salesforce component uri
+ */
 export function buildSalesforceUri(component: string, extension: string): string {
   return `sf://salesforce.com/apexcomponent/${vscode.workspace.getConfiguration('vsforce.organisation')['namespace']}/${component}.${extension}`;
 }
