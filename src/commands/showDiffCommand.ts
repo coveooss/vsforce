d5e67760db132d91ed0d3ccef0af1c1a20fb6195
Lucien Benie <lbenie@coveo.com>
2016-08-04 20:49:00 -0400

    change variable name

--- src/commands/showDiffCommand.ts
+++ src/commands/showDiffCommand.ts
@@ -6,11 +6,11 @@ import {buildSalesforceUri, getFileNameFromUri} from '../utils';
 export class ShowDiffCommand implements Command {
   constructor() {}
 
-  public Command(context: vscode.Uri) {
+  public Command(uri: vscode.Uri) {
     // split[0] == filename
     // split[1] == file extension
-    let split = getFileNameFromUri(context).split('.');
+    let split = getFileNameFromUri(uri).split('.');
     vscode.commands.executeCommand('vscode.diff',
-      context, vscode.Uri.parse(buildSalesforceUri(split[0], split[1])));
+      uri, vscode.Uri.parse(buildSalesforceUri(split[0], split[1])));
   }
 }
