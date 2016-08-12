c5119c8df6b25c5ef1f72da87ecc95d7d45731a5
erocheleau <etienne.rocheleau@gmail.com>
2016-08-12 18:48:38 -0400

    Refactored retrieveCommand

--- src/connection.ts
+++ src/connection.ts
@@ -257,73 +257,6 @@ export class Connection {
   /**
    * TODO: give a description
    *
-   * @param {string} packageXMLPath TODO: give a description
-   */
-  public retrievePackage(packageXMLPath: string) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(packageXMLPath, 'utf-8', (err: NodeJS.ErrnoException, data: Buffer) => {
-        if (err) { reject(err); } else { resolve(data); }
-      });
-    })
-      .then((data) => {
-        return new Promise((resolve, reject) => {
-          xml2js.parseString(data.toString(), { explicitArray: false }, (err, dom) => {
-            if (err) { reject(err); } else { resolve(dom); }
-          });
-        });
-      }, (reason: NodeJS.ErrnoException) => {
-        vscode.window.showErrorMessage(reason.message);
-      })
-      .then((dom: any) => {
-        delete dom.Package.$;
-        let options = {
-          unpackaged: dom.Package
-        };
-        this.retrieve(options)
-          .then((resp: any) => {
-            let outputConsole = vscode.window.createOutputChannel('Retrieve output');
-            outputConsole.show();
-            outputConsole.appendLine('Retrieve request completed');
-            outputConsole.appendLine(`Status: ${resp.status}`);
-            outputConsole.appendLine('============================\n');
-
-            if (resp && resp.messages && resp.messages.length > 0) {
-              resp.messages.forEach(message => {
-                outputConsole.appendLine(`${message.fileName} => `);
-                outputConsole.appendLine(message.problem);
-              });
-            }
-
-            // unzip
-            if (resp && resp.success) {
-              this.extractZip(resp.zipFile, packageXMLPath.replace('package.xml', ''))
-                .then((data) => {
-                  console.log(data);
-                }, (reason: any) => {
-                  vscode.window.showErrorMessage(reason);
-                });
-            } else { outputConsole.appendLine('No output file... Request failed'); }
-
-          }, (reason: any) => {
-            vscode.window.showErrorMessage(reason);
-          });
-      }, (reason: NodeJS.ErrnoException) => {
-        vscode.window.showErrorMessage(reason.message);
-      });
-
-    // fs.readFile(packageXMLPath, (err: NodeJS.ErrnoException, data: Buffer) => {
-    //   xml2js.parseString(data.toString(), (err: any, results: any) => {
-    //     _this.execute((jsforce: any) => {
-    //       console.log(jsforce.metadata.retrieve({ unpackaged: JSON.stringify(results.Package) }).stream().pipe());
-    //       // .pipe(fs.createWriteStream('MyPackage.zip'))
-    //     });
-    //   });
-    // });
-  }
-
-  /**
-   * TODO: give a description
-   *
    * @param {any} content zip file content
    * @param {string} target folder to extract into
    *
@@ -331,10 +264,6 @@ export class Connection {
    */
   private extractZip(content: any, target: string): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      // let waits=[];
-      // let zipStream = new stream.PassThrough();
-      // zipStream.end(new Buffer())
-      // let waits = [];
       let zipStream = new stream.PassThrough();
       zipStream.end(new Buffer(content, 'base64'));
       zipStream.pipe(unzip.Extract({ path: target }));
@@ -349,13 +278,13 @@ export class Connection {
   }
 
   /**
-   * TODO: give a description
+   * Send a retrieve request to salesforce, with options included.
    *
-   * @param {any} options TODO: give a description
+   * @param {any} options The retrieve options : https://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_retrieve_request.htm
    *
-   * @return {Thenable<any>} TODO: give a description
+   * @return {Thenable<any>} Resolves when the requests complete.
    */
-  private retrieve(options: any): Thenable<any> {
+  public retrievePackage(options: any): Thenable<any> {
     return new Promise<any>((resolve, reject) => {
       Connection.getConn().then((conn: Connection) => {
         conn.jsforceConn.metadata.timeout = 60 * 1000;
